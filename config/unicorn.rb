45d5c65d703e0cf25738ae8b4df60929f5036951
Thakur <thakurballary@gmail.com>
2017-06-12 17:43:21 +0530

    Deploy 2

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,10 +1,11 @@
-working_directory "/home/ubuntu/first-react-project"
-pid "/home/ubuntu/first-react-project/tmp/pids/unicorn.pid" 
 
-stderr_path "/home/ubuntu/first-react-project/log/unicorn.log" 
+working_directory "/home/ubuntu/first-react-project"
+pid "/home/ubuntu/first-react-project/tmp/pids/unicorn.pid"
+
+stderr_path "/home/ubuntu/first-react-project/log/unicorn.log"
 stdout_path "/home/ubuntu/first-react-project/log/unicorn2.log"
 
 
-listen "/tmp/unicorn.tada.sock" 
-worker_processes 1
+listen "/tmp/unicorn.tada.sock"
+worker_processes 2
 timeout 240
